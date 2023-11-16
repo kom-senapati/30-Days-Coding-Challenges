75098acf14e1b19df106ae794de257f3bcb547b3
Kom Senapati <komnoob123@gmail.com>
2023-11-16 06:40:42 +0000

    day-11

---
+++ Day-11/solution.js
@@ -0,0 +1,16 @@
+// Challenge 11
+// Write a JavaScript program to list 
+// the properties of a JavaScript object.
+// Sample object:
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+// Sample Output: name,sclass,rollno
+
+var student = {
+name : "David Rayy",
+sclass : "VI",
+rollno : 12 };
+
+console.log(Object.keys(student).join(','))
