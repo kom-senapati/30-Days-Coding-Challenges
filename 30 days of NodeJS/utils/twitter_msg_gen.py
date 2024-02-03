b8b1cd939b3049d7dc7be0cf758e4772538fda09
kom-senapati <komnoob123@gmail.com>
2024-02-03 17:20:54 +0530

    util: NodeJS: X message generator update

--- 30 days of NodeJS/utils/twitter_msg_gen.py
+++ 30 days of NodeJS/utils/twitter_msg_gen.py
@@ -1,17 +1,17 @@
 from random import choice
 message_templates = [
-    "🌟 Survived Day # in the 30 Days of Node.js on Scaler Discord!\n 🚀 Ready for challenges, Node.js exploration, and connecting with enthusiasts! https://bit.ly/scalerdiscord #30DaysOfNodejs #ScalerDiscord @scaler_official",
-    "👋 Day # conquered in the 30 Days of Node.js on Scaler Discord!\n 🚀 Dive into challenges, explore Node.js, and engage with enthusiasts! https://bit.ly/scalerdiscord #30DaysOfNodejs #ScalerDiscord @scaler_official",
-    "🌈 Navigated Day # in the 30 Days of Node.js on Scaler Discord!\n 🚀 Tackle challenges, delve into Node.js, and connect with enthusiasts! https://bit.ly/scalerdiscord #30DaysOfNodejs #ScalerDiscord @scaler_official",
-    "🎉 Triumphed Day # in the 30 Days of Node.js on Scaler Discord!\n 🚀 Ready for adventure, Node.js exploration, and connecting with enthusiasts! https://bit.ly/scalerdiscord #30DaysOfNodejs #ScalerDiscord @scaler_official",
-    "👊 Day # mastered in the 30 Days of Node.js on Scaler Discord!\n 🚀 Dive into challenges, Node.js exploration, and connect with enthusiasts! https://bit.ly/scalerdiscord #30DaysOfNodejs #ScalerDiscord @scaler_official",
-    "🚀 Navigated Day # in the 30 Days of Node.js on Scaler Discord!\n 🌟 Embark on challenges, explore Node.js, and connect with enthusiasts! https://bit.ly/scalerdiscord #30DaysOfNodejs #ScalerDiscord @scaler_official",
-    "💪 Conquered Day # in the 30 Days of Node.js on Scaler Discord!\n 🚀 Ready for more challenges, Node.js exploration, and connecting with enthusiasts! https://bit.ly/scalerdiscord #30DaysOfNodejs #ScalerDiscord @scaler_official",
-    "🌠 Triumphed Day # in the 30 Days of Node.js on Scaler Discord!\n 🚀 Embark on the journey, explore Node.js, and connect with enthusiasts! https://bit.ly/scalerdiscord #30DaysOfNodejs #ScalerDiscord @scaler_official",
-    "👏 Completed Day # in the 30 Days of Node.js on Scaler Discord!\n 🚀 Join the adventure, tackle challenges, and connect with enthusiasts! https://bit.ly/scalerdiscord #30DaysOfNodejs #ScalerDiscord @scaler_official"
+    "🌟 Survived Day # in the 30 Days of Node.js on Scaler Discord!\n 🚀 Ready for challenges, Node.js exploration, and connecting with enthusiasts!",
+    "👋 Day # conquered in the 30 Days of Node.js on Scaler Discord!\n 🚀 Dive into challenges, explore Node.js, and engage with enthusiasts!",
+    "🌈 Navigated Day # in the 30 Days of Node.js on Scaler Discord!\n 🚀 Tackle challenges, delve into Node.js, and connect with enthusiasts!",
+    "🎉 Triumphed Day # in the 30 Days of Node.js on Scaler Discord!\n 🚀 Ready for adventure, Node.js exploration, and connecting with enthusiasts!",
+    "👊 Day # mastered in the 30 Days of Node.js on Scaler Discord!\n 🚀 Dive into challenges, Node.js exploration, and connect with enthusiasts!",
+    "🚀 Navigated Day # in the 30 Days of Node.js on Scaler Discord!\n 🌟 Embark on challenges, explore Node.js, and connect with enthusiasts!",
+    "💪 Conquered Day # in the 30 Days of Node.js on Scaler Discord!\n 🚀 Ready for more challenges, Node.js exploration, and connecting with enthusiasts!",
+    "🌠 Triumphed Day # in the 30 Days of Node.js on Scaler Discord!\n 🚀 Embark on the journey, explore Node.js, and connect with enthusiasts!",
+    "👏 Completed Day # in the 30 Days of Node.js on Scaler Discord!\n 🚀 Join the adventure, tackle challenges, and connect with enthusiasts!"
 ]
 
 day = input("Day > ")
 print()
 print("Copy and post at twitter 👇\n")
-print(choice(message_templates).replace('#',day))
+print(choice(message_templates).replace('#',day) + "https://bit.ly/scalerdiscord #30DaysOfNodejs #ScalerDiscord @scaler_official")
